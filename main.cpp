18cd635cf877dc47d641981871f0f11dc9c84a2b
Gasper Koprivc <gasper.koprivc1@gmail.com>
2026-03-19 11:57:40 +0100

    Add input reading with range validation

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <vector>
 
 int main(int argc, char* argv[]) {
     if (argc != 2) {
@@ -11,6 +12,17 @@ int main(int argc, char* argv[]) {
     if (!input.is_open()) {
         std::cerr << "napaka odpiranje" << std::endl;
         return 1;
+    }
+
+    std::vector<unsigned char> numbers;
+    int x;
+
+    while (input >> x) {
+        if (x < 0 || x > 255) {
+            std::cerr << "ni dovoljen obseg: " << x << std::endl;
+            return 1;
+        }
+        numbers.push_back(static_cast<unsigned char>(x));
     }
 
     input.close();
